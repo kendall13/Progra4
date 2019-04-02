0c3e21b3756b217c89f2b7e72f046aad793e73b2
Kendall Acuña <kendall.23@hotmail.es>
2019-04-02 01:15:06 -0600

    aa

---
+++ CifradoCesar/CifradoCesar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CifradoCesar
+{
+    class Program
+    {
+        static string abc = "abcdefghijklmñnopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+
+        static void Main(string[] args)
+        {
+            //String mensaje = "Cada vez que escucho hablar de ese gato, empiezo a sacar mi pistola.";
+
+            
+            
+            Console.ReadKey();
+
+
+
+
+        }
+
+        static int GetPosABC(char caracter)
+        {
+            for (int i = 0; i < abc.Length; i++)
+            {
+                if(caracter == abc[i]){
+                    return i;
+                }
+            }
+                return -1;
+        }
+
+
+
+
+    }
+}
